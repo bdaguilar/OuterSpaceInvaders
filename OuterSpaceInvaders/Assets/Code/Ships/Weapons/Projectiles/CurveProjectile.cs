7e2a29008d3e1a124936deb30644115e76370ac1
Bruno Daniel Aguilar Lopez <brunodanielaguilarlopez@gmail.com>
2023-07-18 14:19:35 -0600

    Implementing Template method pattern to projectiles

---
+++ OuterSpaceInvaders/Assets/Code/Ships/Weapons/Projectiles/CurveProjectile.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class CurveProjectile : Projectile
+{
+    [SerializeField]
+    private AnimationCurve _horizontalPosition;
+
+    private Vector3 _currentPosition;
+    private float _currentTime;
+
+    protected override void DoStart()
+    {
+        _currentPosition = _transform.position;
+        _currentTime = 0;
+    }
+
+    protected override void DoMove()
+    {
+        _currentPosition += _transform.up * (_speed * Time.deltaTime);
+        Vector3 horizontalPosition = _transform.right * _horizontalPosition.Evaluate(_currentTime);
+        _rigidbody2D.MovePosition(_currentPosition + horizontalPosition);
+
+        _currentTime += Time.deltaTime;
+    }
+
+    protected override void DoDestroyIn()
+    {
+    }
+}
+
+
--- OuterSpaceInvaders/Assets/Code/Ships/Weapons/Projectiles/LinealProjectile.cs
+++ OuterSpaceInvaders/Assets/Code/Ships/Weapons/Projectiles/LinealProjectile.cs
@@ -1,34 +1,20 @@
 ﻿using System.Collections;
 using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody2D))]
+
 public class LinealProjectile : Projectile
 {
-    [SerializeField]
-    private float _speed;
-    [SerializeField]
-    private Rigidbody2D _rigidbody2D;
-    [SerializeField]
-    private Transform _transform;
-    [SerializeField]
-    private float _bulletLifeSpan;
-
-    private void Awake()
-    {
-        _rigidbody2D = GetComponent<Rigidbody2D>();
-        _transform = GetComponent<Transform>();
-    }
-
-    private void Start()
+    protected override void DoStart()
     {
         _rigidbody2D.velocity = _transform.up * _speed;
-        StartCoroutine(DestroyIn(_bulletLifeSpan));
     }
 
-    private IEnumerator DestroyIn(float seconds)
+    protected override void DoMove()
     {
-        yield return new WaitForSeconds(seconds);
-        Destroy(gameObject);
+    }
+
+    protected override void DoDestroyIn()
+    {
     }
 }
 
